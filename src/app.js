be76683f2703742d6df3ad05ec4ed1286ad94e8f
zun3007work <zun3007.work@gmail.com>
2023-09-08 16:47:38 +0700

    Add Cors feature

--- src/app.js
+++ src/app.js
@@ -3,16 +3,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 
 // API version
 const v1 = require('./api/v1');
 
 const app = express();
 
+// CORS
+app.use(cors());
+
 // Security
 app.use(helmet());
-
-// CORS
 
 // Body Parser
 app.use(bodyParser.json());
